2101e559eb6a439f375af74761c67077e11c8719
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-05-06 20:27:55 +0200

    Add nheko to common packages

--- hosts/common/packages.nix
+++ hosts/common/packages.nix
@@ -15,6 +15,7 @@ in
   environment.systemPackages = with pkgs; [
     (python3.withPackages python-packages)
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    nheko
     neovim
     typst
     cm_unicode
