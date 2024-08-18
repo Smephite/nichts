0ba27e6d3620ed541af2500d7100ed6c67e7da51
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-08-18 15:52:21 +0200

    Update nvidia settings to reflect renamed config options

--- options/common/gpu/nvidia.nix
+++ options/common/gpu/nvidia.nix
@@ -2,27 +2,30 @@
 {
   # taken from https://github.com/bloxx12/nichts/blob/main/options/common/gpu/nvidia.nix
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
     # package = pkgs-unstable.mesa.drivers;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
   hardware.nvidia = {
     modesetting.enable = true;
     open = false;
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
     nvidiaSettings = false;
   };
 
+
+
   
   environment.sessionVariables = lib.mkIf config.modules.WM.hyprland.enable {
+    LIBVA_DRIVER_NAME = "nvidia";
     XDG_SESSION_TYPE = "wayland";
-    #GBM_BACKEND = "nvidia-drm";
+    GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     NIXOS_OZONE_WL = "1";
-    WLR_NO_HARDWARE_CURSORS = "1";
+    # WLR_NO_HARDWARE_CURSORS = "1";
     MOZ_ENABLE_WAYLAND = "1";
     SDL_VIDEODRIVER = "wayland";
     #_JAVA_AWT_WM_NONREPARENTING = "1";
