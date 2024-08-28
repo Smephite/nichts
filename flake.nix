67a39c92177d7b67e7230e28c207b20fb0e278a1
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-08-28 14:50:14 +0200

    Remove unused flake inputs

--- flake.nix
+++ flake.nix
@@ -10,55 +10,25 @@
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
-    /*
-    anyrun = {
-        url = "github:Kirottu/anyrun";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
-    */
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nur = {
-      url = "github:nix-community/NUR";
-      #inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # agenix.url = "github:ryantm/agenix";
     stylix.url = "github:danth/stylix";
-    /*
-    schizofox = {
-        url = "github:schizofox/schizofox";
-        inputs = {
-            nixpkgs.follows = "nixpkgs";
-        };
-    };
-    */
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    /*
-    fenix = {
-        url = "github:nix-community/fenix";
-        inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    */
 
-    /*
-    split-monitor-workspaces = {
-      url = "github:Duckonaut/split-monitor-workspaces";
-      #            inputs.hyprland.follows = "hyprland";
-    };
-    */
   };
 }
