e6620927cca4430660a38ca540b92c5ec9a1bb50
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-08-28 14:28:02 +0200

    Move steam config option from flocke/configuration.nix to actual steam config

--- modules/gui/steam.nix
+++ modules/gui/steam.nix
@@ -15,6 +15,15 @@ in {
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
         gamescopeSession.enable = mkIf cfg.gamescope true;
+        # set LD_PRELOAD to correctly load everything for steam: see https://github.com/ROCm/ROCm/issues/2934
+        # TODO: check if this is still relevant
+        package = pkgs.steam.overrideAttrs (prevAttrs: {
+          nativeBuildInputs = (prevAttrs.nativeBuildInputs or []) 
+                            ++ [ pkgs.makeBinaryWrapper ];
+          postInstall = (prevAttrs.postInstall or "") + ''
+            wrapProgram $out/bin/steam --set LD_PRELOAD "${pkgs.libdrm}/lib/libdrm_amdgpu.so"
+          '';
+        });
     };
     home-manager.users.${username} = {
     };
