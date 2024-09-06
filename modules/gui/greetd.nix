4bdbe7a5931b1aad2de2e5e0a3ffe5b3ee792419
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-09-06 11:07:54 +0200

    Change TTY of tuigreete so systemd logs don't pollute the UI

--- modules/gui/greetd.nix
+++ modules/gui/greetd.nix
@@ -20,6 +20,7 @@ in {
     services.greetd = {
       enable = true;
       settings = {
+        terminal.vt = 2; # set to 2 so the systemd logs don't clutter the ui
         default_session = {
           command = ''
             ${pkgs.greetd.tuigreet}/bin/tuigreet \
