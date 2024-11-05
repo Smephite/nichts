fa1f8b2c5e2b721528799e64f5b247311cbf1bee
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-11-05 09:16:30 +0100

    Disable animations in hyprland

--- modules/gui/WM/hyprland.nix
+++ modules/gui/WM/hyprland.nix
@@ -117,7 +117,7 @@ in {
           decoration.rounding = 5;
           misc.disable_hyprland_logo = true;
           animations = {
-            enabled = true;
+            enabled = false;
             # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
             bezier = ["myBezier, 0.05, 0.9, 0.1, 1.05"];
