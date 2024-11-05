67b426aa4ca1aab1907e1cfaa713a9829bdb25aa
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-11-05 09:16:18 +0100

    do not start zellij automatically

--- modules/cli/zellij.nix
+++ modules/cli/zellij.nix
@@ -12,7 +12,7 @@ in {
     home-manager.users.${username} = {
       programs.zellij = {
         enable = true;
-        enableFishIntegration = true;
+        # enableFishIntegration = true;
         settings = {
           #$ on_force_close = "quit";
           pane_frames = false;
@@ -32,7 +32,7 @@ in {
         };
       };
       # TODO: move this somewhere else
-      programs.foot.settings.main.shell = "${pkgs.zellij}/bin/zellij";
+      # programs.foot.settings.main.shell = "${pkgs.zellij}/bin/zellij";
     };
   };
 }
