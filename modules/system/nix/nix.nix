f27c163c5895eef8c6493951a2ee4032e5e9e63d
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-11-06 11:06:19 +0100

    Remove dirty warnings for nix

--- modules/system/nix/nix.nix
+++ modules/system/nix/nix.nix
@@ -20,6 +20,7 @@
       substituters = [
         "https://helix.cachix.org" # a cache for helix
       ];
+      warn-dirty = false;
       trusted-public-keys = [
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       ];
