6430c88610a6f3f4b257ab42cbd0425e599c2fea
Kai Berszin <mail@kaibersz.in>
2025-07-07 11:53:08 +0200

    :wrench: Add bitwarden to firefox

--- modules/gui/browsers/firefox.nix
+++ modules/gui/browsers/firefox.nix
@@ -116,12 +116,21 @@ in {
           OfferToSaveLogins = false;
           font = "Lexend";
           ExtensionSettings = mkMerge [
+            # Ublock origin
             {
               "uBlock0@raymondhill.net" = {
                 install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
                 installation_mode = "force_installed";
               };
             }
+            # Bitwarden
+            {
+              "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+                install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+                installation_mode = "force_installed";
+              };
+            }
+            # TODO: get from config.programs.firefox.extenstions
           ];
         };
       };
