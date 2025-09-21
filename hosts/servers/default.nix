0e228e31e00aa95313264c5d6f2982b9fde0d49f
Kai Berszin <mail@kaibersz.in>
2025-09-21 10:17:38 +0000

    Add backup ssh key

--- hosts/servers/default.nix
+++ hosts/servers/default.nix
@@ -10,5 +10,5 @@
   ];
 
    services.openssh.enable = true;
-   users.users.root.openssh.authorizedKeys.keys = config.authorizedKeys.default;
+   users.users.root.openssh.authorizedKeys.keys = config.authorizedKeys.default ++ [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPnDKM5e9Ukn3dJEW6UzoryatqXgg9uqmiOQSA1ubG1k8MDISvmgisxaCPjk5qUaHSijc5amMAZytVLrcHHPmb716w7CitVvu3As/s+sSjbgVCroiugvdSO25sE8UXrLS1JqC2pBvdqgjgYiR8QHaxlkpUCaIfyWsBG4CwU9QZcxX5ZBCKLCKZr8ntSEWRMyywxjfhZxSPjE+wwPoqSwBhmIhFjFV6vhrQ/gxgfuS/+Sp/HfpbT93lOuxXxiB4PL5qGabh+Udp3Q6S6uhV/Bx7oEPJA7hlVPkU1g2+pJBu76UglpnHnTW0l8SqXv9lm6Pu13o3ZfaXfyw4adoIBa2V Kai@Kai-PC" ];
 }
