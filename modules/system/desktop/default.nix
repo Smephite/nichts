354497591f3e292f85ba7ef3b2cc9ad5c5c3ca68
Kai Berszin <mail@kaibersz.in>
2025-11-27 13:52:07 +0100

    WIP

--- modules/system/desktop/default.nix
+++ modules/system/desktop/default.nix
@@ -1,6 +1,26 @@
 {
+  config,
+  ...
+}:
+  let desktops = with config.modules.system.desktop; [
+    gnome.enable
+    niri.enable
+  ];
+in 
+{
   imports = [
     ./gnome.nix
+    ./niri.nix
     ./monitors.nix
   ];
+
+  config = {
+    assertions = [
+      {
+        assertion = ((builtins.length (builtins.filter (x: x) desktops)) <= 1);
+        message = "Only one option from config.modules.sytem.desktop may be enabled at a time!";
+      }
+    ]; 
+  };
+
 }
