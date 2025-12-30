524b2562c8ca2c4d12f27b8a00fafef49afd764d
Kai Berszin <mail@kaibersz.in>
2025-12-30 13:50:20 +0100

    Fix rebase error

--- flake.nix
+++ flake.nix
@@ -19,11 +19,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
