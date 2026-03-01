6f9d12bc263869f40ae139c841be020725c2d5fa
Kai Berszin <mail@kaibersz.in>
2026-03-01 17:05:59 +0100

    Fork zeditor

--- overlay.nix
+++ overlay.nix
@@ -1,24 +1,29 @@
-{inputs, ...}: let
+{ inputs, ... }:
+let
   add_nylon_pr = final: prev: {
-    inherit
-      (inputs.nixpkgs-nylon-wg.legacyPackages.${prev.system})
+    inherit (inputs.nixpkgs-nylon-wg.legacyPackages.${prev.system})
       nylon-wg
       ;
   };
-  add_librepods_pr = final: prev: {
-    librepods = inputs.nixpkgs-librepods.legacyPackages.${prev.system}.librepods.overrideAttrs (_: rec{
-    #  src = prev.fetchFromGitHub{
-    #    owner = "kavishdevar";
-    #    repo = "librepods";
-    #    rev = "a01e16792a73deb34c5bd0c4aa019c496642ee71"; # linux/rust
-    #    hash = "sha256-ZvHbSSW0rfcsNUORZURe0oBHQbnqmS5XT9ffVMwjIMU=";
-    #  };
-    });
-    
+  add_zed_fork = final: prev: {
+    zed-editor = inputs.zed-fork.packages.${prev.system}.default;
   };
-in {
+  add_librepods_pr = final: prev: {
+    librepods = inputs.nixpkgs-librepods.legacyPackages.${prev.system}.librepods.overrideAttrs (_: rec {
+      #  src = prev.fetchFromGitHub{
+      #    owner = "kavishdevar";
+      #    repo = "librepods";
+      #    rev = "a01e16792a73deb34c5bd0c4aa019c496642ee71"; # linux/rust
+      #    hash = "sha256-ZvHbSSW0rfcsNUORZURe0oBHQbnqmS5XT9ffVMwjIMU=";
+      #  };
+    });
+
+  };
+in
+{
   nixpkgs.overlays = [
     add_nylon_pr
     add_librepods_pr
+    add_zed_fork
   ];
 }
