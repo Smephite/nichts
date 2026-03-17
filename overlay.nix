0205a68e4ba0b8ca11d437891e201a9eb9670871
Kai Berszin <mail@kaibersz.in>
2026-03-17 15:32:44 +0100

    Update packages and use default zed

--- overlay.nix
+++ overlay.nix
@@ -9,14 +9,16 @@ let
     zed-editor = inputs.zed-fork.packages.${prev.stdenv.hostPlatform.system}.default;
   };
   add_librepods_pr = final: prev: {
-    librepods = inputs.nixpkgs-librepods.legacyPackages.${prev.stdenv.hostPlatform.system}.librepods.overrideAttrs (_: rec {
-      #  src = prev.fetchFromGitHub{
-      #    owner = "kavishdevar";
-      #    repo = "librepods";
-      #    rev = "a01e16792a73deb34c5bd0c4aa019c496642ee71"; # linux/rust
-      #    hash = "sha256-ZvHbSSW0rfcsNUORZURe0oBHQbnqmS5XT9ffVMwjIMU=";
-      #  };
-    });
+    librepods =
+      inputs.nixpkgs-librepods.legacyPackages.${prev.stdenv.hostPlatform.system}.librepods.overrideAttrs
+        (_: rec {
+          #  src = prev.fetchFromGitHub{
+          #    owner = "kavishdevar";
+          #    repo = "librepods";
+          #    rev = "a01e16792a73deb34c5bd0c4aa019c496642ee71"; # linux/rust
+          #    hash = "sha256-ZvHbSSW0rfcsNUORZURe0oBHQbnqmS5XT9ffVMwjIMU=";
+          #  };
+        });
 
   };
 in
@@ -24,6 +26,6 @@ in
   nixpkgs.overlays = [
     add_nylon_pr
     add_librepods_pr
-    add_zed_fork
+    #add_zed_fork
   ];
 }
