c33f8c767990651ac42b61f0063ad86dc16abb3d
Kai Berszin <mail@kaibersz.in>
2026-04-18 13:45:20 +0200

    feat: add zed and garnix binary caches
    Add extra-substituters and extra-trusted-public-keys for
    zed.cachix.org and cache.garnix.io to avoid building zed from source.

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -2,12 +2,28 @@
   inputs,
   lib,
   pkgs,
+  self,
+  config,
   ...
-}: {
+}:
+{
   # partly taken from github.com/bloxx12/nichts
 
+  age.secrets.github-ro-token.file = "${self}/secrets/github-ro.age";
+
   nix = {
+    extraOptions = ''
+      !include ${config.age.secrets.github-ro-token.path}
+    '';
     settings = {
+      extra-substituters = [
+        "https://zed.cachix.org"
+        "https://cache.garnix.io"
+      ];
+      extra-trusted-public-keys = [
+        "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
       extra-experimental-features = [
         "flakes" # flakes
         "nix-command" # experimental nix commands
