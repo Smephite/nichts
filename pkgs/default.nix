abf2aeb60d420bca5d8fee9f2c57146c4bc2c328
Kai Berszin <mail@kaibersz.in>
2026-04-22 15:01:26 +0200

    Refactor pkgs structure

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,8 +4,8 @@
   withPdf ? true,
   withScite ? true,
 }: {
-  pyzotero = pkgs.python3Packages.callPackage ./pyzotero.nix {};
-  zotero-mcp = pkgs.python3Packages.callPackage ./zotero-mcp.nix {
+  pyzotero = pkgs.python3Packages.callPackage ./pyzotero {};
+  zotero-mcp = pkgs.python3Packages.callPackage ./zotero-mcp {
     pyzotero = pkgs.pyzotero;
     inherit withSemantic withPdf withScite;
   };
