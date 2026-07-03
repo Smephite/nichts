0aab99d6663b276f5ead88f3803b1b2c32a7f643
Kai Berszin <mail@kaibersz.in>
2026-07-03 18:49:32 +0200

    Add work monitor profile for silverwind

--- hosts/silverwind/configuration.nix
+++ hosts/silverwind/configuration.nix
@@ -47,7 +47,53 @@
           gnome-calendar.enable = true;
         };
         windowManager = "cosmic";
-        monitors = [];
+        monitorGroups.work-externals = [
+          {
+            name = "HP";
+            model = "HP E27u G4";
+            resolution = {
+              x = 2560;
+              y = 1440;
+            };
+            refresh_rate = 59.951;
+            position = {
+              x = 1440;
+              y = 0;
+            };
+          }
+          {
+            name = "ASUS";
+            model = "BE27A";
+            resolution = {
+              x = 2560;
+              y = 1440;
+            };
+            refresh_rate = 59.951;
+            position = {
+              x = 4000;
+              y = 0;
+            };
+          }
+        ];
+        monitors.work = {
+          groups = ["work-externals"];
+          extra = [
+            {
+              name = "laptop";
+              device = "eDP-1";
+              resolution = {
+                x = 2880;
+                y = 1920;
+              };
+              refresh_rate = 120.0;
+              scale = 2.0;
+              position = {
+                x = 0;
+                y = 0;
+              };
+            }
+          ];
+        };
       };
     };
     programs = {
